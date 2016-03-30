6b5eacbc00182fb12cc9b9a35e920dad8b8f8089
tieskedh <thijsdehaan71@hotmail.com>
2016-03-30 11:46:56 +0200

    adds gui

---
+++ src/main/java/gui/AbstractModel.java
@@ -0,0 +1,34 @@
+package gui;
+
+/**
+ * This class defines the data the model needs to provide
+ * Created by thijs on 24-3-2016.
+ */
+public abstract class AbstractModel {
+
+
+    /**
+     * All the sets where the current player can place a piece.
+     * @return an array of points where the player can place their  piece
+     */
+    public abstract int[] getValidSets();
+
+    /**
+     * returns the point where the current piece is placed
+     * @return the point of the current place
+     */
+    public abstract int getSetLocation();
+
+    /**
+     * returns the side of the player which is  currently placed
+     * @return the current player
+     */
+    public abstract int getSide();
+
+    /** eventID at the start of the game */
+    public static final int TURN_START = 0;
+    /** eventID at the end of the game */
+    public static final int TURN_END = 1;
+    /** eventID at the placement of a piece */
+    public static final int PLACE_PIECE = 2;
+}
---
+++ src/main/java/gui/GameView.java
@@ -0,0 +1,93 @@
+package gui;
+
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.stream.IntStream;
+
+/**
+ * The gui for tictactoe
+ * Created by Thijs de Haan on 24-3-2016.
+ */
+public class GameView extends JPanel implements ActionListener{
+
+    /** the representation for the players by their name*/
+    private final HashMap<Integer, String> players;
+    /** the fields of the view*/
+    private JButton[] buttons;
+    private LinkedList<ActionListener> actionListeners = new LinkedList<>();
+    private int size;
+
+    /**
+     * Creates the GameView for tictactoe
+     * @param width the number of fields in the width
+     * @param height the number of fields in the height
+     * @param players the representation of the players by their id
+     */
+    public GameView(int width, int height, Map<Integer, String> players) {
+        super(new GridLayout(width, height, 10, 10));
+        size = width*height;
+        this.players = new HashMap<>(players);
+        buttons = new JButton[width*height];
+        IntStream.range(0, size)
+                .forEach(nr-> {
+                    JButton button = new JButton();
+                    button.setEnabled(true);
+                    button.addActionListener(this::buttonPressed);
+                    buttons[nr] = button;
+                    add(button);
+                });
+        setVisible(true);
+    }
+
+    /**
+     * Action to perform when a button is pressed
+     * @param actionEvent the event which occured
+     */
+    private void buttonPressed(ActionEvent actionEvent) {
+        JButton button = (JButton) actionEvent.getSource();
+        int nummer = IntStream.range(0, size)
+                .filter(nr -> button==buttons[nr])
+                .findFirst().getAsInt();
+        fireEvent(new ActionEvent(this, nummer, "PRESSED"));
+    }
+
+    private void fireEvent(ActionEvent event) {
+        actionListeners.forEach(listener -> listener.actionPerformed(event));
+    }
+
+    private void addActionListener(ActionListener listener) {
+        actionListeners.add(listener);
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        AbstractModel model = (AbstractModel) e.getSource();
+
+        switch (e.getID()) {
+            case AbstractModel.TURN_START:
+                int[] validSets = model.getValidSets();
+                Arrays.stream(validSets)
+                        .forEach(
+                                nr-> buttons[nr]
+                                        .setEnabled(true)
+                        );
+                break;
+            case AbstractModel.TURN_END:
+                Arrays.stream(buttons)
+                        .forEach(button->button.setEnabled(false));
+                break;
+            case AbstractModel.PLACE_PIECE:
+                int place = model.getSetLocation();
+                JButton button = buttons[place];
+                button.setText(players.getOrDefault(model.getSide(), ""));
+        }
+    }
+}
